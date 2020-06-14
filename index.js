7742d4b551e86e0c394f1df9dd688875a7a3375a
sarahayu <46186848+sarahayu@users.noreply.github.com>
2020-06-13 18:36:32 -0700

    Added token json, refactored index

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 quotesJSON = require('./quotes.json');
+token = require('./token.json').token;
 keywords = [];
 aliases = [];
 quotes = [];
 mutedServers = [];
+
+const log = message => {
+  console.log(`[${new Date().toLocaleString()}] ${message.author.username} sent: ${message.content}`);
+}
+
+const getRemainingMins = cid => {
+  return Math.ceil((mutedServers[cid] - new Date()) / 60000);
+}
+
+const sendInfo = user => {
+  let table = '```markdown\n< Commands >\n!rb shutup [minutes] OR !rb mute [minutes] to mute me temporarily\n!rb unmute to unmute me\n!rb status to get status\n\n< Current Media Sources and Trigger Words >\n';
+
+  for (let key in aliases)
+    if (aliases.hasOwnProperty(key))
+      table += key.padEnd(45) + aliases[key] + '\n';
+
+  user.send(table + '```');
+}
+
+const getUntil = minutes => {
+  let until = new Date();
+  until.setMinutes(until.getMinutes() + minutes);
+  until = new Date(until);
+  return until;
+}
 
 for (let i = 0; i < quotesJSON.quotes.length; i++)
 {
@@ -18,107 +44,91 @@ for (let i = 0; i < quotesJSON.quotes.length; i++)
 
 client.on('ready', () => {
   let clientUser = client.user;
-  clientUser.setActivity("Type !rb for info");
-  console.log('Hullo! From ' + clientUser.username + ' (' + clientUser.id + ')');
+  clientUser.setActivity('Type !rb for info');
+  console.log(`Hullo! From ${clientUser.username} (${clientUser.id})`);
 })
 
-client.on("guildCreate", guild => {
-    let channelID;
-    let channels = guild.channels.cache;
-    channelLoop:
-    for (let c in channels) {
-        if (channels[c].type === "text") {
-            channelID = channels[c].id;
-            break channelLoop;
-        }
+client.on('guildCreate', guild => {
+  let cid;
+  let channels = guild.channels.cache;
+
+  for (let c in channels)
+    if (channels[c].type === 'text')
+    {
+      cid = channels[c].id;
+      break;
     }
 
-    let channel = client.channels.cache.get(guild.systemChannelID || channelID);
-    channel.send("I detect trigger words and spout nonsense quotes from movies, vines, and the like\nType `!rb` to see what quotes I have in store");
+  let channel = client.channels.cache.get(guild.systemChannelID || cid);
+  channel.send('I detect trigger words and spout nonsense quotes from movies, vines, and the like\nType `!rb` to see what quotes I have in store');
 });
 
 client.on('message', message => {
-	if (message.author.bot) return;
-  
+  if (message.author.bot) return;
+
+  log(message);
+
   const channel = message.channel;
+  const cid = channel.id;
   const messageStr = message.content.toLowerCase();
 
-  if (messageStr.substring(0, 3) === "!rb")
+  if (messageStr.startsWith('!rb'))
   {
-    let args = messageStr.split(" ");
-    
-    if (args.length >= 2 && (args[1] === "shutup" || args[1] === "mute") && (args.length == 2 || !isNaN(args[2])))
-    {
-      let mins = 1;
-      if (args.length == 3) 
-        mins = parseInt(args[2])
-      let until = new Date();
-      until.setMinutes(until.getMinutes() + mins);
-      until = new Date(until);
-      mutedServers[message.channel.id] = until;
-      channel.send("You have now muted me for " + mins + " minute(s). Type `!rb status` to check how many more minutes remain before I unleash havoc once more");
-    }
-    else if (args.length == 2 && args[1] === "unmute")
-    {
-      delete mutedServers[message.channel.id];
-      channel.send("I am now unmuted and ready to wreak havoc");
-    }
-    else if (args.length == 2 && args[1] === "status")
-    {
-      let muted = false;
+    let args = messageStr.split(/ +/); args.shift();
 
-      if (message.channel.id in mutedServers)
+    if (args.length >= 1 && (args[0] === 'shutup' || args[0] === 'mute') && (args.length == 1 || !isNaN(args[1])))
+    {
+      let mins = args.length == 2 ? parseInt(args[1]) : 1;
+      mutedServers[cid] = getUntil(mins);
+
+      channel.send(`You have now muted me for ${mins} minute(s). Type \`!rb status\` to check how many more minutes remain before I unleash havoc once more`);
+    }
+    else if (args.length == 1 && args[0] === 'unmute')
+    {
+      delete mutedServers[cid];
+      channel.send('I am now unmuted and ready to wreak havoc');
+    }
+    else if (args.length == 1 && args[0] === 'status')
+    {
+      if (cid in mutedServers)
       {
-        let remainingMins = Math.ceil((mutedServers[message.channel.id] - new Date()) / 60000);  
-        if (remainingMins <= 0)
-          delete mutedServers[message.channel.id];
-        else
-        {
-          muted = true;
-          channel.send("I'm muted for " + remainingMins + " more minute(s)! Type `!rb unmute` to unmute me");
-        }
-      }
-          
-      if (!muted)
-        channel.send("I am not muted and ready to wreak havoc");
-    }
-    else if (args.length == 1)
-    {
-      let table = "```markdown\n< Commands >\n!rb shutup [minutes] OR !rb mute [minutes] to mute me temporarily\n!rb unmute to unmute me\n!rb status to get status\n\n< Current Media Sources and Trigger Words >\n";
+        let remainingMins = getRemainingMins(cid);
 
-      for (let key in aliases)
-        if (aliases.hasOwnProperty(key))
-          table += key.padEnd(45) + aliases[key] + "\n";
-      message.author.send(table + "```");
+        if (remainingMins > 0)
+        {
+          channel.send(`I'm muted for ${remainingMins} more minute(s)! Type\`!rb unmute\` to unmute me`);
+          return;
+        }
+
+        delete mutedServers[cid];
+      }
+
+      channel.send('I am not muted and ready to wreak havoc');
     }
+    else if (!args.length)
+      sendInfo(message.author);
     else
-      channel.send("I'm sorry, I don't recognize that command. If you're unsure, type `!rb` for information")
+      channel.send(`I'm sorry, I don't recognize that command. If you're unsure, type\`!rb\` for information`)
   }
   else
   {
-    let unmuted = true;
-
-    if (message.channel.id in mutedServers)
+    if (cid in mutedServers)
     {
-      let remainingMins = Math.ceil((mutedServers[message.channel.id] - new Date()) / 60000);
+      let remainingMins = getRemainingMins(cid);
 
-      if (remainingMins <= 0)
-        delete mutedServers[message.channel.id];
-      else
-        unmuted = false;
+      if (remainingMins > 0) return;
+      delete mutedServers[cid];
     }
-    
-    if (unmuted)
-    {
-      for (const keyword in keywords)
-        if (messageStr.includes(keyword))
-        {
-          let possibleQuotes = quotes[keywords[keyword]];
-          channel.send(possibleQuotes[Math.floor(Math.random() * possibleQuotes.length)]);
-        }
-    }
+
+    for (const keyword in keywords)
+      if (messageStr.includes(keyword))
+      {
+        let possibleQuotes = quotes[keywords[keyword]];
+        channel.send(possibleQuotes[Math.floor(Math.random() * possibleQuotes.length)]);
+        return;   // return on first instance to avoid spamming multiple quotes at once
+      }
   }
 
 });
 
-client.login(TOKEN);
+client.login(token);
